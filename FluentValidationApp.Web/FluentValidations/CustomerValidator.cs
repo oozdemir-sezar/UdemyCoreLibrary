c21cb53797d729f115e6f673de4413eac902ca77
oguzhan.ozdemir <oguzhan.ozdemir@hotmail.com>
2020-12-11 05:39:08 +0300

    Add project files.

---
+++ FluentValidationApp.Web/FluentValidations/CustomerValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FluentValidationApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidationApp.Web.FluentValidations
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+
+        public CustomerValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name alanı boş olamaz");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email alanı boş olamaz").EmailAddress().WithMessage("Email alanı doğru formatta değil");
+            RuleFor(x => x.Age).NotEmpty().WithMessage("Age alanı boş olamaz").InclusiveBetween(18, 60).WithMessage("Age alanı 18-60 arasında olmalıdır");
+        }
+    }
+}
