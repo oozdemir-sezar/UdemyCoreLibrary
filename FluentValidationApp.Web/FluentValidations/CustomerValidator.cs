08ca0ff186aa82a670e35c01fd5600647fb620b3
oguzhan.ozdemir <oguzhan.ozdemir@hotmail.com>
2020-12-11 05:46:39 +0300

    commit 1

--- FluentValidationApp.Web/FluentValidations/CustomerValidator.cs
+++ FluentValidationApp.Web/FluentValidations/CustomerValidator.cs
@@ -13,7 +13,9 @@ namespace FluentValidationApp.Web.FluentValidations
         public CustomerValidator()
         {
             RuleFor(x => x.Name).NotEmpty().WithMessage("Name alanı boş olamaz");
+
             RuleFor(x => x.Email).NotEmpty().WithMessage("Email alanı boş olamaz").EmailAddress().WithMessage("Email alanı doğru formatta değil");
+
             RuleFor(x => x.Age).NotEmpty().WithMessage("Age alanı boş olamaz").InclusiveBetween(18, 60).WithMessage("Age alanı 18-60 arasında olmalıdır");
         }
     }
